9dc5ca0d6f43fb3252938c62d0b3e9a97b837c3c
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-04-17 17:56:48 +0200

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Carlos Román Alcaide on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
